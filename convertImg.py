b9d35193bee915f6355119d78066684228c23667
Tsuyoshi-py <henri.tsuyoshi@hotmail.com>
2021-04-03 06:46:46 -0300

    add readme and boundary

--- convertImg.py
+++ convertImg.py
@@ -1,7 +1,7 @@
 import data
 import numpy
 
-def to_num(matrix, chars_per_line, boundary):
+def to_num(matrix, chars_per_line, boundary, boundaryhl):
     num_array = [0] * chars_per_line
 
     line_size = len(matrix)
@@ -9,8 +9,12 @@ def to_num(matrix, chars_per_line, boundary):
 
     for i in range(line_size):
         for j in range(column_size):
-            if matrix[i][j] > boundary:
-                num_array[i // 2] = num_array[i // 2] + 2 ** ((j * 2) + (i % 2))
+            if boundaryhl == 'l':
+                if matrix[i][j] < boundary:
+                    num_array[i // 2] = num_array[i // 2] + 2 ** ((j * 2) + (i % 2))
+            else:
+                if matrix[i][j] > boundary:
+                    num_array[i // 2] = num_array[i // 2] + 2 ** ((j * 2) + (i % 2))
 
     return num_array
 
@@ -23,19 +27,18 @@ def to_matrix(array, height_index, width):
             array_index = array_index + 1
     return matrix
 
-def px_ascii_art(array, width, height):
+def px_ascii_art(array, width, height, boundary, boundaryhl):
 
     width = width - width % 2
     height = height - height % 4
     number_of_lines = height // 4
     ascii_art = ""
-    boundary = 128
 
     height_index = 0
     while number_of_lines > 0:
         matrix = to_matrix(array, height_index, width)
-        num_array = to_num(matrix, width // 2, boundary)
-        
+        num_array = to_num(matrix, width // 2, boundary, boundaryhl)
+
         for number in num_array:
             ascii_art = ascii_art + chr(data.braileData.get(number))
 
--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import convertImg
 import PIL.Image
 
 path = 'Write the path for the file: '
-choose = 'Choose the image width (recommended size: 100):'
+choose = 'Choose the image width (recommended size: 100): '
+boundary1 = '(Default = 128) Choose the boundary (0-255): '
+boundary2 = 'Paint if the value is higher (h) or lower (l) than boundary: '
+exception1 = 'Width has to be higher than 0'
+exception2 = 'Boundary has to be a number'
+exception3 = 'Boundary has to be lower than 256 and higher than -1'
+exception4 = "The input has to be 'h' or 'l'"
 
 def resize_image(image, new_width=100):
     width, height = image.size
@@ -21,12 +27,32 @@ if __name__ == '__main__':
         exit()
 
     new_image_width = int(input(choose))
+    if new_image_width < 0:
+        raise Exception(exception1)
+
     image = resize_image(image, new_image_width)
+
+    boundary = input(boundary1)
+    if boundary != '':
+        if not boundary.isnumeric():
+            raise Exception(exception2)
+        boundary = int(boundary)
+        if boundary > 255 or boundary < 0:
+            raise Exception(exception3)
+    else:
+        boundary = 128
+
+    boundaryhl = input(boundary2)
+    if boundaryhl != '':
+        if boundaryhl != 'h' and boundaryhl != 'l':
+            raise Exception(exception4)
+    else:
+        boundaryhl = 'h'
 
     width, height = image.size
     px_array = gray_pixels(image)
-    ascii_art = convertImg.px_ascii_art(px_array, width, height)
-    output_file = open("waifu.txt", "w")
+    ascii_art = convertImg.px_ascii_art(px_array, width, height, boundary, boundaryhl)
+    output_file = open('waifu.txt', 'w')
     output_file.write(ascii_art)
 
     print(ascii_art)
